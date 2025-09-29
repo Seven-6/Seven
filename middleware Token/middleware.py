32d205834aef4003fb3257c3495fed7da4c85240
miaoqingkun <miaoqingkun@qianxin.com>
2025-09-29 15:48:54 +0800

    fix

--- middleware Token/middleware.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger('default')
-
-
-class ExceptionLoggingMiddleware(object):
-    def process_exception(self, request, exception):
-        import traceback
-        logger.error(traceback.format_exc())
